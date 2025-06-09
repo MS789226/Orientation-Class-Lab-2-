3e0551e14959865e87014bad4c9541356f8bb49f
MS789226 <thyhmbyers@gmail.com>
2025-06-08 19:40:08 -0700

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,42 @@
+Employee.java
+
+
+ class Employee {
+   
+    private int employeeId;
+    private String firstName;
+    private String lastName;
+
+    
+     getEmployeeId() {
+        return employeeId;
+    }
+
+     setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+  
+     String getFirstName() {
+        return firstName;
+    }
+
+      setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    
+     String getLastName() {
+        return lastName;
+    }
+
+      setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    
+    
+     String toString() {
+        return "Employee ID: " + employeeId + ", Name: " + firstName + " " + lastName;
+    }
+}
