7f67a096339804a3d0c2c0982be6b6e37ce94c5e
MS789226 <thyhmbyers@gmail.com>
2025-06-08 19:41:49 -0700

    Create Matsidiso.java

---
+++ Matsidiso.java
@@ -0,0 +1,20 @@
+Matsidiso.Java
+java.util.Scanner;
+
+Scanner scanner = new Scanner(System.in);
+              
+ Employee employee = new Employee();
+               
+ System.out.print("Enter Employee ID: ");
+ employee.setEmployeeId(scanner.nextInt()); 
+ System.out.print("Enter First Name: ");
+ employee.setFirstName(scanner.nextLine());
+        
+ System.out.print("Enter Last Name: ");
+ employee.setLastName(scanner.nextLine());
+               
+  System.out.println(employee);
+        
+  scanner.close();
+    }
+}
